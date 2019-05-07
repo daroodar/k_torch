50ef1950562a0ffeaf5cff6b60b78087aeb62f09
OSAMA DAR <osama.dar@skyelectric.com>
2019-05-07 15:10:31 +0500

    improved code with initializers file being called now

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="k_torch",
-    version="0.0.1",
+    version="0.0.2",
     author="Osama dar, Muhammad Ismail",
     author_email="osamadar1996@gmail.com",
     description="A Keras-like wrapper for PyTorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/daroodar/k_torch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
